77007d43b3f09aa862318b9504f80fb3ae68d9dd
Gabriel Marin <marinjhow@gmail.com>
2026-02-06 00:59:25 -0300

    feat: implements swagger

---
+++ src/main/java/dev/java10x/gamesearch/config/SwaggerConfig.java
@@ -0,0 +1,28 @@
+package dev.java10x.gamesearch.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI getOpenAPI() {
+
+        Contact contact = new Contact();
+        contact.url("https://www.linkedin.com/in/m9rin/");
+        contact.email("marinjhow@gmail.com");
+
+        Info info = new Info();
+        info.title("Game Search");
+        info.version("v1");
+        info.description("Aplicação desenvolvida para cadastro e pesquisa de jogos e suas plataformas disponiveis");
+        info.contact(contact);
+
+
+        return new OpenAPI().info(info);
+    }
+}
