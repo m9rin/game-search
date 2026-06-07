367aef332492e077e1bcbf87c0245fac0684fd36
Gabriel Marin <marinjhow@gmail.com>
2026-06-07 11:27:32 -0300

    refactor: generated seed and add .env.example

--- src/main/java/dev/java10x/gamesearch/config/SecurityConfig.java
+++ src/main/java/dev/java10x/gamesearch/config/SecurityConfig.java
@@ -38,10 +38,7 @@ public class SecurityConfig {
                 .build();
     }
 
-    @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
-        return authenticationConfiguration.getAuthenticationManager();
-    }
+
 
     @Bean
     public PasswordEncoder passwordEncoder() {
